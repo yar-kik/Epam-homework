87742854fab502d264c2954f8bbf672ad7ce38ac
Yaroslav Kikvadze <60779044+yar-kik@users.noreply.github.com>
2021-07-10 16:08:24 +0300

    Update task_10_ex_1.py

--- task10/task_10_ex_1.py
+++ task10/task_10_ex_1.py
@@ -16,8 +16,8 @@ Xavier
 def sort_names(input_file_path: str, output_file_path: str) -> None:
     """Sorts names from `file_path` and write them to a new file
     `output_file_path`."""
-    with open(f"{input_file_path}", 'r') as input_file, \
-            open(f"{output_file_path}", 'w') as output_file:
+    with open(input_file_path, 'r') as input_file, \
+            open(output_file_path, 'w') as output_file:
         all_names = input_file.read().strip()
         names_list = all_names.split("\n")
         sorted_names = "\n".join(sorted(names_list)) + '\n'
